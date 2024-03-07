b568ae43309e5d4225c6e2957126a450cbe75adc
Jester-1987 <nicole.groeger@triosstudent.com>
2024-03-07 11:48:46 -0500

    silenced alert message

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-alert('You are the [ERROR]th visitor! There is no prize, just the satisfaction of visting this webbed site')
+// alert('You are the [ERROR]th visitor! There is no prize, just the satisfaction of visting this webbed site')
